0b553d1aeff3b1026da1767c5f539f036c95b7a8
Jonas Røssum <jonaswehoo@gmail.com>
2019-10-28 21:08:17 +0100

    work on 3D stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Canvas,
+  useFrame,
+  extend,
+  useRender,
+  useThree
+} from 'react-three-fiber'
+import React, { useRef } from 'react'
+import './App.css'
+import { Vector3, Euler } from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+extend({ OrbitControls })
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Canvas
+      style={{
+        width: '100vw',
+        height: '100vh'
+      }}>
+      <Thing />
+      <Controls />
+    </Canvas>
+  )
 }
 
-export default App;
+function Thing() {
+  // const ref = useRef()
+  // useFrame(
+  //   () =>
+  //     // ref.current.rotation.x =
+  //     (ref.current.rotation.y += 0.01)
+  // )
+  return (
+    <>
+      <mesh
+        // ref={ref}
+        onClick={e => {
+          debugger
+        }}
+        position={new Vector3(0, 3, 0)}
+        onPointerOver={e => console.log('hover')}
+        onPointerOut={e => console.log('unhover')}>
+        <boxBufferGeometry attach="geometry" args={[4, 0.1, 1]} />
+        <meshNormalMaterial attach="material" />
+      </mesh>
+      {/* <mesh
+        ref={ref}
+        onClick={e => {
+          debugger
+        }}
+        position={new Vector3(0, 0, 3)}
+        onPointerOver={e => console.log('hover')}
+        onPointerOut={e => console.log('unhover')}>
+        <boxBufferGeometry attach="geometry" args={[3, 0.25, 5]} />
+        <meshNormalMaterial attach="material" />
+      </mesh> */}
+      <mesh
+        // ref={ref}
+        onClick={e => {
+          debugger
+        }}
+        position={new Vector3(0, 0, 0)}
+        rotation={new Euler(1, 0, 3, 'XYZ')}
+        onPointerOver={e => console.log('hover')}
+        onPointerOut={e => console.log('unhover')}>
+        {/* <boxBufferGeometry attach="geometry" args={[4, 0.1, 1]} /> */}
+        <planeGeometry
+          // center={new Vector3(0, 0, 0)}
+          attach="geometry"
+          args={[5, 5]}
+        />
+        <meshNormalMaterial attach="material" />
+      </mesh>
+    </>
+  )
+}
+
+function Controls(props) {
+  const ref = useRef()
+  const { camera } = useThree()
+  useRender(() => ref.current.update())
+  return <orbitControls ref={ref} args={[camera]} {...props} />
+}
+
+export default App
